5a83c6b36a1e7c6ae96bed9eac022679cd3647a7
g-allen23 <geovaneallen8@gmail.com>
2025-02-05 23:50:30 -0300

    Alterações no index.html, outras alterações no styles.css, e começando a adicionar um carrossel de imagens no scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const forms = document.querySelector(".form")
 const mascara = document.querySelector(".mask")
+const rightArrow = document.getElementById("right")
 
 function show(){
     forms.style.left = "50%"
@@ -13,3 +14,25 @@ function toHide(){
     forms.style.transform = "translateX(-20%)"
     mascara.style.visibility = "hidden"
 }
+
+function changeImageRight() {
+    const imgMain = document.querySelector(".carousel")
+
+    if (imgMain.src.includes("teste.jpg")) {
+        imgMain.src = "assets/teste2.jpg"
+    } 
+    
+    else if (imgMain.src.includes("teste2.jpg")) {
+        imgMain.src = "assets/teste3.jpg"
+    }
+
+    else if (imgMain.src.includes("teste3.jpg")) {
+        imgMain.src = "assets/teste4.jpg"
+    }
+
+    else {
+        imgMain.src = "assets/teste.jpg"
+    }
+}
+
+rightArrow.addEventListener("click", changeImageRight)
