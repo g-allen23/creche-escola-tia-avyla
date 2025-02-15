77c35103a3fb59c1441f339635f39dfa473c6efd
g-allen23 <geovaneallen8@gmail.com>
2025-02-14 22:04:11 -0300

    Adicionado mais uma função em script.js e algumas alterações em styles.css

--- scripts.js
+++ scripts.js
@@ -1,27 +1,28 @@
 const forms = document.querySelector(".form")
 const mascara = document.querySelector(".mask")
 const rightArrow = document.getElementById("right")
+const leftArrow = document.getElementById("left")
+const imgMain = document.querySelector(".carousel")
 
-function show(){
+function show() {
     forms.style.left = "50%"
     forms.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
 }
 
 
-function toHide(){
+function toHide() {
     forms.style.left = "-300px"
     forms.style.transform = "translateX(-20%)"
     mascara.style.visibility = "hidden"
 }
 
 function changeImageRight() {
-    const imgMain = document.querySelector(".carousel")
 
     if (imgMain.src.includes("teste.jpg")) {
         imgMain.src = "assets/teste2.jpg"
-    } 
-    
+    }
+
     else if (imgMain.src.includes("teste2.jpg")) {
         imgMain.src = "assets/teste3.jpg"
     }
@@ -35,4 +36,24 @@ function changeImageRight() {
     }
 }
 
+function changeImageLeft() {
+
+    if (imgMain.src.includes("teste4.jpg")) {
+        imgMain.src = "assets/teste3.jpg"
+    }
+
+    else if (imgMain.src.includes("teste3.jpg")) {
+        imgMain.src = "assets/teste2.jpg"
+    }
+
+    else if (imgMain.src.includes("teste2.jpg")) {
+        imgMain.src = "assets/teste.jpg"
+    }
+
+    else {
+        imgMain.src = "assets/teste4.jpg"
+    }
+}
+
+leftArrow.addEventListener("click", changeImageLeft)
 rightArrow.addEventListener("click", changeImageRight)
